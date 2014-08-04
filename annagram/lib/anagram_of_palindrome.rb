e3520450336cf88165111ea03f4f13fa8fa1c153
dancingshell <kaitlinjaffe@gmail.com>
2014-08-04 01:21:22 -0700

    refactored annagram to put everything inside one while loop

--- annagram/lib/anagram_of_palindrome.rb
+++ annagram/lib/anagram_of_palindrome.rb
@@ -4,9 +4,10 @@ def solution(s)
   i = 0
   odd_counter = 0
   large_counter = 0
-  if arr.length % 2 == 0
-    while i < arr.length
-      dup = arr.count(arr[i])
+
+  while i < arr.length
+    dup = arr.count(arr[i])
+    if arr.length % 2 == 0
       if dup % 2 == 0 && dup > 0
         i += 1
       elsif dup > 1 && dup % 2 == 1
@@ -16,10 +17,7 @@ def solution(s)
         palindrone = false
         break
       end
-    end
-  else
-    while i < arr.length
-      dup = arr.count(arr[i])
+    else
       if dup % 2 == 0 && dup > 0
         i += 1
       elsif dup <= 1 && odd_counter == 0
@@ -32,8 +30,8 @@ def solution(s)
         palindrone = false
         break
       end
-    end
-    palindrone == false if large_counter % 2 == 1
+    end  
   end
+  palindrone = false if large_counter % 2 == 1
   palindrone == false ? 0 : 1
 end
